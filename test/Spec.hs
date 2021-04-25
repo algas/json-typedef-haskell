82d700bcec20c5e7449f50375929140af68f4890
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2021-04-25 13:32:43 +0900

    update test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.HUnit
+import Data.JTD.Base
+
+-- test1 = TestCase (assertEqual "hello test" 1 1)
+test1 = TestCase $ do
+          validationData <- decodeJSON "./validation.json"
+          assertBool "validation" True
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  runTestTTAndExit $ TestList [TestLabel "test1" test1]
